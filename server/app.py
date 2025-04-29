e7bc12ea57328df34782d06a160dc7ed3678bd05
Arthur Guillemin <aguillemin291@gmail.com>
2025-04-29 15:23:57 +0200

    ➕ Feat : Add rate limit  ➕

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 CORS(app)
 
+limiter = Limiter(
+    get_remote_address, 
+    app=app,
+    default_limits=["10 per minute"],
+)
 
 # Import routes
 
@@ -15,14 +22,6 @@ from routes.auth_routes import auth_routes
 # Register routes
 app.register_blueprint(user_routes, url_prefix="/users")
 app.register_blueprint(auth_routes, url_prefix="/auth")
-
-
-
-
-
-
-
-
 app.register_blueprint(test_routes, url_prefix="/test")
 
 
