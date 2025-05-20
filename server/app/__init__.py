b0a0ec4f4b4c932f13c347b96a8cd388a11c9194
Arthur Guillemin <aguillemin291@gmail.com>
2025-05-20 22:24:10 +0200

    🔐 Secure headers avec Talisman middleware fix JWT_SECRET_KEY, gestion CORS et config dotenv

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,6 +4,10 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
+from flask_talisman import Talisman
+
+import os
+load_dotenv()
 
 from .config import Config
 
@@ -11,7 +15,6 @@ from app.routes.auth_routes import auth_routes
 from app.routes.user_routes import user_routes
 from app.routes.test_routes import test_routes
 
-load_dotenv()
 
 jwt = JWTManager()
 limiter = Limiter(key_func=get_remote_address)
@@ -29,5 +32,10 @@ def create_app():
     app.register_blueprint(auth_routes, url_prefix="/auth")
     app.register_blueprint(user_routes, url_prefix="/users")
     app.register_blueprint(test_routes, url_prefix="/test")
+
+    #talisman
+    is_prod = os.getenv("FLASK_ENV") == "production"
+    Talisman(app, content_security_policy=None , force_https=is_prod) 
+
 
     return app
