21bce1d1710f678cc51ecae9ec7ce5f5d7490e2b
Arthur Guillemin <aguillemin291@gmail.com>
2025-05-28 14:53:21 +0200

    ✉️ Add deployemnt link✉️

--- server/app/config.py
+++ server/app/config.py
@@ -6,6 +6,6 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
     CORS_ORIGINS = [
         "http://localhost:5173",
-        "https://682c61b0f8a2f90008bba62c--gomiproject.netlify.app"
+        "https://gomiproject.netlify.app"
     ]
     RATELIMIT_DEFAULT = "10 per minute"
